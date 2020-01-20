f965b827f3cc459ba1e0ad2e37c3c48848c246a1
Guillaume Meurisse <g.meurisse.ia@gmail.com>
2020-01-20 11:54:22 +0100

    Add prototype of functions

--- scrapper/scrap.py
+++ scrapper/scrap.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from annonce import Annonce
+
 from bs4 import BeautifulSoup
 
 with open("test.html", "r") as f:    
     contents = f.read()
     soup = BeautifulSoup(contents, 'html.parser')
     print(soup.prettify())
+
+    
+def scrap_annonce(url):
+    """Scrap a page of a property"""
+
+    annonce = Annonce(**{'idannonce':12345, 'prix':12345})
+    return annonce
+
+def scrap_search_page(num_page):
+    """Scrap the search page in order to find properties to scrap"""
+
+    urls = []
+    #for ****
+    #    if idannonce n'est pas dans la BDD:
+    #        urls.append(url)
+    return urls
