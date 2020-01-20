f6a4192354c7b09d85a895a5fc2b00372d28a311
Guillaume Meurisse <g.meurisse.ia@gmail.com>
2020-01-20 13:28:13 +0100

    Add argument parsing

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-
+import argparse
 from annonce import Annonce, to_csv
 #from query import ***
-from scrap import  scrap_annonce, scrap_search_page
+#from scrap import  scrap_annonce, scrap_search_page
 
 # scrap_search_page(num_page)
 # need function to test Annonce exist in bdd
@@ -12,10 +12,25 @@ from scrap import  scrap_annonce, scrap_search_page
 
 # need function to get all annonces from BDD
 
+parser = argparse.ArgumentParser(description='Scrap and manage scrapped data')
+cmd_subparser = parser.add_subparsers(title='command', dest='cmd')
+
+scrap_parser = cmd_subparser.add_parser('scrap', help='')
+csv_parser = cmd_subparser.add_parser('csv', help='')
+csv_parser.add_argument('--file', help='filename', required=True)
+
+args = parser.parse_args()
+
+
 def scrap():
     pass
 
-def db_to_csv():
-    pass
+def db_to_csv(filename):
+    print(filename)
 
 
+if args.cmd == 'scrap':
+    scrap()
+
+elif args.cmd == 'csv':
+    db_to_csv(args.file)
