80c4964b2c734eef83e5c66fa4d0eb3b070fe3f2
Pierre <38954574+pierremirandebroucas@users.noreply.github.com>
2020-03-13 10:06:29 +0100

    Add API to app

--- prediction_container/www/app.py
+++ prediction_container/www/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import joblib
 import logging as log
 from pandas import DataFrame
@@ -40,6 +40,39 @@ def predict():
     output = round(model.predict(values)[0],0)
 
     return render_template('resultats.html', prix = output, typedebien = request.form['typedebien'])
+
+@app.route('/api', methods=['GET'])
+def predict_api():
+    input_list = [[
+        request.json['typedebien'],
+        request.json['codepostal'],
+        request.json['idtypechauffage'],
+        request.json['idtypecuisine'],
+        request.json['si_balcon'],
+        request.json['nb_chambres'],
+        request.json['nb_pieces'],
+        request.json['si_sdbain'],
+        request.json['si_sdEau'],
+        request.json['etage'],
+        request.json['surface'],
+        request.json['dpeC']
+    ]]
+    
+    columns = ['typedebien', 'codepostal', 'idtypechauffage', 'idtypecuisine',
+           'si_balcon', 'nb_chambres', 'nb_pieces', 'si_sdbain', 'si_sdEau',
+           'etage', 'surface', 'dpeC']
+    values = DataFrame(data=input_list,columns=columns)
+    
+    file_path = max(glob(path.join('/app/www/models', 'realestate-model-*.pkl')))    
+    model = joblib.load(open(file_path, 'rb'))
+    output = round(model.predict(values)[0],0)
+    
+    return jsonify({
+        'statut' : 'OK',
+        'prix' : output
+    },
+    200
+    )
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
