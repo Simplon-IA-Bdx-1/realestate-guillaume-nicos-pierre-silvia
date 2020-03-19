328fd61fb852049251f5db5f132dfdc64a3764dd
Guillaume Meurisse <g.meurisse.ia@gmail.com>
2020-03-19 14:00:25 +0100

    Create .env file for db econnction

--- selenium_scrapper_container/init.sh
+++ selenium_scrapper_container/init.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+cat > /app/python/.env <<EOF
+MYSQL_HOST=${MYSQL_HOST}
+MYSQL_USER=${MYSQL_USER}
+MYSQL_PASSWORD=${MYSQL_PASSWORD}
+MYSQL_DATABASE=${MYSQL_DATABASE}
+EOF
+
+echo "Scheduled tasks:" && crontab -l
 echo "starting cron service"
 cron
 
