da69891ada630337e02f023a1fe4a02f0bc3aa78
Nicolas SEVERINO <nicolasseverino@outlook.com>
2020-03-23 10:53:23 +0100

    Add new table 'current_model' related to table 'models'

--- BDD/init-db/03-model-database.sql
+++ BDD/init-db/03-model-database.sql
@@ -20,3 +20,19 @@ PRIMARY KEY (`model_name`)
 
 
 
+
+-- ************************************** `current_model`
+
+CREATE TABLE `current_model`
+(
+ `id`         enum(1) NOT NULL ,
+ `model_name` varchar(255) NOT NULL ,
+
+PRIMARY KEY (`id`),
+KEY `fkIdx_125` (`model_name`),
+CONSTRAINT `FK_125` FOREIGN KEY `fkIdx_125` (`model_name`) REFERENCES `models` (`model_name`)
+);
+
+
+
+
