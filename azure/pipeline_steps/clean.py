2e5e822684122eecc05fb5fcaac34ca186afc985
Guillaume Meurisse <g.meurisse.ia@gmail.com>
2020-03-31 18:33:30 +0200

    Remove bug caused by nan replaced by empty string

--- azure/pipeline_steps/clean.py
+++ azure/pipeline_steps/clean.py
@@ -2,6 +2,7 @@ import argparse
 from azureml.core import Run, Dataset
 from os import path
 import numpy as np
+import pandas as pd
 
 output_dir = './Outputs'
 
@@ -15,17 +16,20 @@ run_context = Run.get_context()
 input_dataset = run_context.input_datasets[args.input]
 df_full = input_dataset.to_pandas_dataframe()
 
-chauffageNArows = df_full['idtypechauffage'] == 0
+chauffageNArows = (df_full['idtypechauffage'] == '0') | (df_full['idtypechauffage'] == '') | (df_full['idtypechauffage'] == 'l')
 df_full.loc[chauffageNArows,'idtypechauffage'] = np.nan
+df_full['idtypechauffage'] = df_full['idtypechauffage'].replace(r'^z','gaz', regex=True)
 
 codpostal33rows = df_full['codepostal'] == 33
 df_full.loc[codpostal33rows,'codepostal'] = 33000
 
-cuisineNArows = df_full['idtypecuisine'] == 0
+cuisineNArows = (df_full['idtypecuisine'] == '0') | (df_full['idtypecuisine'] == '')
 df_full.loc[cuisineNArows,'idtypecuisine'] = np.nan
+df_full['idtypecuisine'] = df_full['idtypecuisine'].replace(r'^éparée','séparée', regex=True)
 
-nonzero_surface = (df_full['surface'] != 0) & (~df_full['surface'].isna())
-df_full = df_full.loc[nonzero_surface, :]
+
+nonzero_surface = (df_full['surface'] != '0') & (df_full['surface'] != '')
+df_full = df_full[nonzero_surface]
 df_full['surface'] = df_full['surface'].str.replace(",", ".").astype(float)
 
 notcolocation_rows = ~(df_full['description'].str.contains("([Cc]oloc)")).astype('Bool')
