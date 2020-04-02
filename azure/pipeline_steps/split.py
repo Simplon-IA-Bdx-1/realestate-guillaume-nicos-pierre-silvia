8f13d5118d85abc0120aa39f3b1ede86b94d97ce
Guillaume Meurisse <g.meurisse.ia@gmail.com>
2020-04-02 13:39:32 +0200

    Complete split.py testing

--- azure/pipeline_steps/split.py
+++ azure/pipeline_steps/split.py
@@ -17,22 +17,23 @@ args = parser.parse_args()
 
 run_context = Run.get_context()
 input_dataset = run_context.input_datasets[args.dataset]
-train = run_context.input_datasets[args.train]
-valid = run_context.input_datasets[args.valid]
-trainsize = run_context.input_datasets[args.trainsize]
-validsize = run_context.input_datasets[args.validsize]
+fulltrain = input_dataset.to_pandas_dataframe()
 
-valid_size = int(validsize)
-train_size = int(len(input_dataset) - valid_size)
+train_size = int(args.trainsize)
+valid_size = int(args.validsize)
 
-train, valid = input_dataset[0:train_size], input_dataset[:-valid_size]
+#valid_size = int(validsize)
+#train_size = int(len(input_dataset) - valid_size)
 
-train.to_csv(path.join(output_dir, args.train))
+if args.train:
+    train = fulltrain[-train_size-valid_size:-valid_size]
+    train.to_csv(path.join(output_dir, args.train))
+valid = fulltrain[-valid_size:]
 valid.to_csv(path.join(output_dir, args.valid))
 
-print('Dataset: ' {}.format(input_dataset))
-print('Train name: ' {}.format(train))
-print('Valid name: ' {}.format(valid))
-print('Train size: ' {}.format(train_size))
-print('Valid size: ' {}.format(valid_size))
+print('Dataset:  {}'.format(input_dataset))
+print('Train name:  {}'.format(train))
+print('Valid name:  {}'.format(valid))
+print('Train size:  {}'.format(train_size))
+print('Valid size:  {}'.format(valid_size))
 
