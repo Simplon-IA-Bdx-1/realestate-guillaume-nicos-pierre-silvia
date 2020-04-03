355ded4b814b86437056cb9e7ce65f2484dae50d
Silvia Ciappelloni <silvia.ciappelloni@gmail.com>
2020-04-03 09:45:20 +0200

    modification on eval

--- azure/pipeline_steps/eval.py
+++ azure/pipeline_steps/eval.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score, mean_squared_error
 from sklearn.model_selection import cross_val_score
-from joblib import load
+import joblib
 
 dataset_dir = './datasets'
 model_dir = './models'
@@ -17,17 +17,19 @@ parser.add_argument('--model', required=True)
 args = parser.parse_args()
 
 model_path = Model.get_model_path(args.model)
-model = joblib.load(model_path)
+
+model = joblib.load(open(model_path, 'rb'))
 
 run_context = Run.get_context()
-evaluation = run_context.log('rmse', valid_rmse, description='Root mean square error on the validation set')
+
 input_dataset = run_context.input_datasets[args.dataset]
-model = input_dataset.to_pandas_dataframe()
+valid_df = input_dataset.to_pandas_dataframe()
 
+target_column = "prix"
+X_valid = valid_df.drop(target_column, axis=1)
+y_valid = valid_df[target_column]
 
-scores = cross_val_score(full_pipe, X_fulltrain, y=y_fulltrain, cv=10)
-
-y_valid_pred = full_pipe.predict(X_valid)
+y_valid_pred = model.predict(X_valid)
 valid_r2 = r2_score(y_valid, y_valid_pred)
 valid_rmse = np.sqrt(mean_squared_error(y_valid, y_valid_pred))
 valid_rmsle = np.sqrt(mean_squared_error(np.log(y_valid),
@@ -35,10 +37,9 @@ valid_rmsle = np.sqrt(mean_squared_error(np.log(y_valid),
 valid_msle = (mean_squared_error(np.log(y_valid), np.log(y_valid_pred)))
 valid_mape = np.mean(np.abs((y_valid-y_valid_pred)/y_valid))
 
-
-model_file_name = 'realestate-model-'\
-                  + now.strftime("%Y-%d-%m-%Hh%Mm")\
-                  + '-mape-'\
-                  + '{:.3f}'.format(valid_mape)\
-                  + '.pkl'
+run_context.log('rmse', valid_rmse, description='Root mean square error on the validation set')
+run_context.log('r2', valid_r2, description='Root square on the validation set')
+run_context.log('rmsle', valid_rmsle, description='Root mean square log error on the validation set')
+run_context.log('msle', valid_msle, description='Mean square log error on the validation set')
+run_context.log('mape', valid_mape, description='Mean absolute percentage error on the validation set')
 
